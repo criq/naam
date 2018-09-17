617782602e11036d9ad4df5230793877f97e026c
Jan Jablunka <jablunka@gmail.com>
2018-09-17 15:54:21 +0200

    incremental

--- src/Naam.php
+++ src/Naam.php
@@ -4,29 +4,90 @@ namespace Naam;
 
 class Naam {
 
-	public function __construct($type, $name, $gender) {
-		$this->type = $type;
-		$this->name = $name;
-		$this->gender = $gender;
+	public function __construct($type, $name, $gender = null) {
+		$this->type = (string)(new \Katu\Types\TString((string)$type))->normalizeSpaces()->trim();
+		$this->name = (string)(new \Katu\Types\TString((string)$name))->normalizeSpaces()->trim();
+		$this->gender = (string)(new \Katu\Types\TString((string)$gender))->normalizeSpaces()->trim();
+	}
+
+	public function getRes($timeout = 2419200) {
+		return \Katu\Cache::get([__CLASS__, __FUNCTION__], $timeout, function($naam) {
+
+			$data = [
+				'type' => $naam->type,
+				'name' => $naam->name,
+			];
+
+			if ($this->gender) {
+				$data['gender'] = $this->gender;
+			}
+
+			$url = \Katu\Types\TUrl::make('http://hi.ondraplsek.cz', $data);
+			$res = \Katu\Cache\Url::get($url);
+
+			if (isset($res->success) && $res->success) {
+				return $res->results;
+			}
+
+			return [];
+
+		}, $this);
+	}
+
+	public function getGenders() {
+		$genders = array_map(function($item) {
+			return $item->gender;
+		}, $this->getRes());
+
+		return $genders;
+	}
+
+	static function getPrevalentGenderFromGenders($genders) {
+		$genderCounts = array_count_values($genders);
+		asort($genderCounts, \SORT_NATURAL);
+		$genderCounts = array_reverse($genderCounts);
+
+		if (!isset($genderCounts['male']) && !isset($genderCounts['female'])) {
+			return false;
+		}
+
+		if (isset($genderCounts['male'], $genderCounts['female']) && $genderCounts['male'] == $genderCounts['female']) {
+			return false;
+		}
+
+		return array_keys($genderCounts)[0];
+	}
+
+	public function getPrevalentGender() {
+		return static::getPrevalentGenderFromGenders($this->getGenders());
+	}
+
+	public function getVocativesByGender($gender) {
+		return array_values(array_filter($this->getRes(), function($item) use($gender) {
+			return $item->gender == $gender;
+		}));
+	}
+
+	public function getVocativesRes() {
+		$res = $this->getRes();
+		if (!$res) {
+			return false;
+		}
+
+		if ($this->gender) {
+			return $this->getVocativesByGender($this->gender);
+		} else {
+			return $this->getVocativesByGender($this->getPrevalentGender());
+		}
 	}
 
 	public function getVocative() {
-		$res = \Katu\Utils\Cache::get(function($type, $name, $gender) {
+		$res = $this->getVocativesRes();
+		if (isset($res[0]->vocativ)) {
+			return $res[0]->vocativ;
+		}
 
-			$curl = new \Curl\Curl;
-			$res = $curl->get('http://hi.ondraplsek.cz', [
-				'name' => $name,
-				'type' => $type,
-				'gender' => $gender,
-			]);
-
-			if (!isset($res->success) || (isset($res->success) && !$res->success)) {
-				return false;
-			}
-
-			var_dump($res->results[0]); die;
-
-		}, 86400 * 28, $hiType, $hiName, $hiGender);
+		return false;
 	}
 
 }
